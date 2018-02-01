c9f3cd32c4b989d4827aa94dbbb32ce61e03a80a
unknown <cormac.o.riordan91@gmail.comgit config --global user.name Etorix>
2018-02-01 20:47:10 +0000

    adding phantom-assert for phantomjs

---
+++ circleci/tests.js
@@ -0,0 +1,14 @@
+var html_fixture = "test.html";
+var sources = ["./assert.js"];
+var tests = [];
+
+tests.push(function check_display () {
+
+    document.getElementById("btn").click();
+    var element = document.getElementById("div"),
+    style = window.getComputedStyle(element),
+    display = style.getPropertyValue("display");
+    assert(display, "block");
+
+})
+
