930635e6b93f00bcd181cdf0fe3749e4095b81ba
unknown <cormac.o.riordan91@gmail.comgit config --global user.name Etorix>
2018-02-01 22:13:05 +0000

    attempt to get past unknown strict host..

--- .circleci/heroku.sh
+++ .circleci/heroku.sh
@@ -3,14 +3,14 @@
   sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
   sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
+  cat > ~/.netrc << EOF
+  machine api.heroku.com
+    yes
+    login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
+  EOF
+
   cat >> ~/.ssh/config << EOF
   VerifyHostKeyDNS yes
   StrictHostKeyChecking no
-  EOF
-
-
-  cat > ~/.netrc << EOF
-  machine api.heroku.com
-    login $HEROKU_LOGIN
-    password $HEROKU_API_KEY
   EOF
